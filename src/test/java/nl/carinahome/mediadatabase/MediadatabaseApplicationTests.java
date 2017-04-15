fa749fd491521e215ef4faad9009b243fad63561
Carina Horrel <Carina Horrel>
2017-04-15 12:59:42 +0200

    my first commit

---
+++ src/test/java/nl/carinahome/mediadatabase/MediadatabaseApplicationTests.java
@@ -0,0 +1,16 @@
+package nl.carinahome.mediadatabase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MediadatabaseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
