6677b669ea153cf3147bb79e389657e8c53b9635
Carina Horrel <Carina Horrel>
2017-04-18 12:19:37 +0200

    adding more stuff css etc

--- src/main/java/nl/carinahome/mediadatabase/config/JerseyConfig.java
+++ src/main/java/nl/carinahome/mediadatabase/config/JerseyConfig.java
@@ -6,6 +6,8 @@ import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.stereotype.Component;
 
 import nl.carinahome.mediadatabase.rest.service.ActorEndpoint;
+import nl.carinahome.mediadatabase.rest.service.ArtistEndpoint;
+import nl.carinahome.mediadatabase.rest.service.CDEndpoint;
 import nl.carinahome.mediadatabase.rest.service.DVDEndpoint;
 import nl.carinahome.mediadatabase.rest.service.GenreEndpoint;
 
@@ -24,8 +26,8 @@ public class JerseyConfig extends ResourceConfig {
 		register(DVDEndpoint.class);
 		register(GenreEndpoint.class);
 		register(ActorEndpoint.class);
-		//register(CDEndpoint.class);
-		//register(ArtistEndpoint.class);
+		register(CDEndpoint.class);
+		register(ArtistEndpoint.class);
 		//register(FileImportEndpoint.class);
 	}
 }
